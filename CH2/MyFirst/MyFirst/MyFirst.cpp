b25876356128165681e74b142897a1f90398b703
cihy999 <cihy999@gmail.com>
2020-05-08 15:24:45 +0800

    完成 CH 2 實作。

---
+++ CH2/MyFirst/MyFirst/MyFirst.cpp
@@ -0,0 +1,13 @@
+﻿// MyFirst.cpp : display a messaage
+
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!";
+	cin.get();
+	return 0;
+}
---
+++ CH2/OutFunc/OutFunc/OutFunc.cpp
@@ -0,0 +1,25 @@
+﻿// OutFunc.cpp : difining your own function
+
+#include <iostream>;
+
+using namespace std;
+
+void Simon(int);
+
+int main()
+{
+	Simon(3);
+	cout << "Pick an integer: ";
+	int count;
+	cin >> count;
+	Simon(count);
+	cout << "Done!" << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}
+
+void Simon(int n) 
+{
+	cout << "Simon says touch your toes " << n << " times" << endl;
+}
