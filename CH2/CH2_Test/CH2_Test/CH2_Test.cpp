b9b0872f781c3f6d53238920a8066a3ad6ad7754
cihy999 <cihy999@gmail.com>
2020-05-13 10:00:28 +0800

    提交 CH2 的測驗內容。

---
+++ CH2/CH2_Test/CH2_Test/CH2_Test.cpp
@@ -0,0 +1,26 @@
+﻿// CH2_Test.cpp : C++ Primer Plus CH 2 程式測驗
+
+#include <iostream>
+
+using namespace std;
+
+void ShowTime(int, int);
+
+int main()
+{
+	cout << "Enter the number of hours: ";
+	int hour;
+	cin >> hour;
+	cout << "Enter the number of minutes: ";
+	int min;
+	cin >> min;
+	ShowTime(hour, min);
+	cin.get();
+	cin.get();
+	return 0;
+}
+
+void ShowTime(int h, int m) 
+{
+	cout << "Time: " << h << ":" << m << endl;
+}
