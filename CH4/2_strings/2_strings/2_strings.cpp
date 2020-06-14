e935de9d06e6809a14afb7bd93a8d3340e274bc7
cihy999 <cihy999@gmail.com>
2020-06-14 21:52:25 +0800

    開始 CH4，完成字元字串相關練習

---
+++ CH4/2_strings/2_strings/2_strings.cpp
@@ -0,0 +1,24 @@
+﻿// 2_strings.cpp : storing strings in an array
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using namespace std;
+    const int ArSize = 15;
+    char name1[ArSize];
+    char name2[ArSize] = "C++owboy";
+
+    cout << "Howdy! I'm " << name2;
+    cout << " ! What's your name?\n";
+    cin >> name1;
+    cout << "Well, " << name1 << ", your name has ";
+    cout << strlen(name1) << " letters and is stored\n";
+    cout << "in an array of " << sizeof(name1) << " bytes.\n";
+    cout << "Your initial is " << name1[0] << ".\n";
+    name2[3] = '\0';
+    cout << "Here are first 3 characters of my name: ";
+    cout << name2 << endl;
+    return 0;
+}
