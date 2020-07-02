73802e9bcc698880c24099d931686e834a6df6ea
cihy999 <cihy999@gmail.com>
2020-07-02 23:04:26 +0800

    完成 基本Pointer和new 的相關練習

---
+++ CH4/14_address/14_address/14_address.cpp
@@ -0,0 +1,16 @@
+﻿// 14_address.cpp : using the & operator to find address
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int donuts = 6;
+    double cups = 4.5;
+
+    cout << "donuts value = " << donuts;
+    cout << " and dounts address = " << &donuts << endl;
+    cout << "cups values = " << cups;
+    cout << " and cups address = " << &cups << endl;
+    return 0;
+}
